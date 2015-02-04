1caaee5e205d8ede43f92b7f64a577b6f1d278b9
Fabrizio Guglielmino <guglielmino@gmail.com>
2015-02-04 22:15:57 +0100

    Prima release beta funzionante

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
-var EXTENSION_ID="365056580234"
+
+var EXTENSION_ID="365056580234";
 var browserId = "";
 var post_url = "http://localhost:8000/browser/register";
 
@@ -14,25 +15,25 @@ function registerCallback(registrationId) {
     }
 
     data_pack["registrationId"] = registrationId;
-	send_data();
+    send_data();
 }
 
 function show(message) {
-	var payload = JSON.parse(message.data.data_dic);
-	new Notification(payload["channel_name"], {
-		icon: '48.png',
-		body: message.data.alert_msg
-	});
+    var payload = JSON.parse(message.data.data_dic);
+    new Notification(payload["channel_name"], {
+        icon: '48.png',
+        body: message.data.alert_msg
+    });
 }
 
 function generateUUID() {
-	var d = new Date().getTime();
-	var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-			var r = (d + Math.random()*16)%16 | 0;
-			d = Math.floor(d/16);
-			return (c=='x' ? r : (r&0x3|0x8)).toString(16);
-	});
-	return uuid;
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return uuid;
 };
 
 
@@ -42,7 +43,7 @@ function generateUUID() {
  * Handler for push notifications
  */
 chrome.gcm.onMessage.addListener(function(message) {
-	show(message);
+    show(message);
 });
 
 /*
@@ -50,68 +51,71 @@ chrome.gcm.onMessage.addListener(function(message) {
  */
 
 
-function send_data(){
-	if( data_pack["channel_name"] && 
-			data_pack["username"] &&
-			data_pack["registrationId"]){
-		 
-			var xhr = new XMLHttpRequest();
-		    xhr.open("POST", post_url, true);
+function send_data() {
+    if (data_pack["channel_name"] &&
+        data_pack["username"] &&
+        data_pack["registrationId"]) {
 
-		    xhr.onreadystatechange = function() {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", post_url, true);
 
-		        if (xhr.readyState == 4) {
-					//console.log(xhr.responseText)
-		        }
-		    }
-		    data = { 
-		    	'channel': data_pack["channel_name"], 
-		    	'token': data_pack["registrationId"], 
-		    	'name': data_pack["username"],
-		    	'browser': 'chrome', 
-		    	'device_id': browserId };
+        xhr.onreadystatechange = function() {
 
-		    xhr.send(JSON.stringify(data));
-	}
+            if (xhr.readyState == 4) {
+                //console.log(xhr.responseText)
+            }
+        }
+        data = {
+            'channel': data_pack["channel_name"],
+            'token': data_pack["registrationId"],
+            'name': data_pack["username"],
+            'browser': 'chrome',
+            'device_id': browserId
+        };
+
+        xhr.send(JSON.stringify(data));
+    }
 }
 
 
 
 chrome.storage.sync.get('brid', function(items) {
-		var brid = items.brid;
-		if (brid) {
-				idAcquired(brid);
-		} else {
-				brid = generateUUID();
-				chrome.storage.sync.set({brid: brid}, function() {
-					idAcquired(brid);
-				});
-		}
-		function idAcquired(brid) {
-			browserId = brid;
-			console.log("id " + brid);
+    var brid = items.brid;
+    if (brid) {
+        idAcquired(brid);
+    } else {
+        brid = generateUUID();
+        chrome.storage.sync.set({
+            brid: brid
+        }, function() {
+            idAcquired(brid);
+        });
+    }
 
-			chrome.runtime.onMessageExternal.addListener(
-			  	function(request, sender, sendResponse) {
-			  		console.log("EXT message received");
-			  		if (request.channelName && request.username){
-			  			data_pack["channel_name"] = request.channelName;
-			  			data_pack["username"] = request.username;
-			  			console.log("Received " + data_pack["channel_name"] + " - " + data_pack["username"]);
+    function idAcquired(brid) {
+        browserId = brid;
+        console.log("id " + brid);
 
-			  			send_data();
-			  		}
-			 });
+        chrome.runtime.onMessageExternal.addListener(
+            function(request, sender, sendResponse) {
+                console.log("EXT message received");
+                if (request.channelName && request.username) {
+                    data_pack["channel_name"] = request.channelName;
+                    data_pack["username"] = request.username;
+                    console.log("Received " + data_pack["channel_name"] + " - " + data_pack["username"]);
 
-			chrome.runtime.onConnectExternal.addListener(function(port) {
-    			console.log("message received");
-			});
+                    send_data();
+                }
+            });
+
+        chrome.runtime.onConnectExternal.addListener(function(port) {
+            console.log("message received");
+        });
 
 
-			// Start of registration process
-			var senderIds = [EXTENSION_ID];
-			chrome.gcm.register(senderIds, registerCallback);
+        // Start of registration process
+        var senderIds = [EXTENSION_ID];
+        chrome.gcm.register(senderIds, registerCallback);
 
-		}
+    }
 });
-
